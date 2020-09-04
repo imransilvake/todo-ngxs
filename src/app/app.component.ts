04faeb573dcfa8c06818af271637d97e820fe466
Imran Khan <imransilvake@gmail.com>
2020-09-04 18:07:17 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-root',
-	templateUrl: './app.component.html'
+	templateUrl: './app.component.html',
+	styles: [
+		`h3 { margin-top: 25px; }`
+	]
 })
 
 export class AppComponent {
